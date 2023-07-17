2037471aec4f1b0bac90f5d56ba24fc124d18008
Barış Atasoy <109789408+the-atasoy@users.noreply.github.com>
2023-07-17 12:09:21 +0300

    First Project in python.

---
+++ RockPaperScissors.py
@@ -0,0 +1,25 @@
+import random
+
+def play():
+    player = input("What's your choice? 'r' for Rock, 'p' for Paper, 's' for Scissors.\n")
+    computer = random.choice(['r', 'p', 's'])
+    print("You are " + player + " and " + "Opponent are " + computer + ".")
+    print(player + "   Vs   " + computer)
+
+    if player == computer:
+        return 'It\'s a tie'
+
+    if is_win(player, computer):
+        return 'You won!'
+    else:
+        return 'You lost!'
+
+def is_win(player, opponent):
+    if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') or (player == 'p' and opponent == 'r'):
+        return True
+
+result = play()
+print("Result ---> " + result)
+
+
+
