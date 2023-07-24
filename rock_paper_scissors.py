0e81095ad72fed49bb7daa522ab213336cac6959
Sevde Secer <sevdeseceerr@gmail.com>
2023-07-24 13:31:49 +0300

    First Project in python.

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -2,6 +2,7 @@ import random
 def play():
     player = input("What's your choice? 'r' for Rock, 'p' for Paper, 's' for Scissors.\n")
     computer = random.choice(['r', 'p', 's'])
+
     print("You are " + player + " and " + "Opponent are " + computer + ".")
     print(player + "   Vs   " + computer)
 
